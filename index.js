788e20e59192ae1d9348b634a22f5e2dbb80967e
Jagrata Deb <jagratadeb2004@gmail.com>
2025-09-04 00:28:00 +0530

    add code for mongodb and list the task

--- index.js
+++ index.js
@@ -1,14 +1,29 @@
 import express from "express";
 import path from 'path';
+import { MongoClient } from "mongodb";
 
 const app = express();
 const publicPath = path.resolve('public');
 app.use(express.static(publicPath));
-
 app.set("view engine",'ejs')
 
-app.get("/", (req, resp) => {
-  resp.render("List");
+const dbName = "todo-project";
+const collectionName = "todo";
+const url = "mongodb://localhost:27017"
+const client = new MongoClient(url);
+
+const connection = async ()=> {
+  const connect = await client.connect();
+  return await connect.db(dbName);
+}
+
+app.use(express.urlencoded({extended:false}))
+
+app.get("/", async (req, resp) => {
+  const db = await connection();
+  const collection = db.collection(collectionName);
+  const result = await collection.find().toArray();
+  resp.render("List", { result });
 });
 
 app.get("/add", (req, resp) => {
@@ -23,7 +38,15 @@ app.post("/update", (req,resp) => {
   resp.redirect("/")
 })
 
-app.post("/add", (req,resp) => {
+app.post("/add",async (req,resp) => {
+  const db = await connection();
+  const collection = db.collection(collectionName)
+  const result = collection.insertOne(req.body)
+  if(result){
+    resp.redirect("/")
+  }else{
+    resp.redirect("/add")
+  }
   resp.redirect("/")
 })
 
